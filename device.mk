eb84700a78238eda4b5d773c5fa62800da443a20
Lokesh Gidra <lokeshgidra@google.com>
2019-12-06 13:58:23 -0800

    Violet: Use 4GB dalvik-heap config
    We are currently using 2GB dalvik-heap config.
    
    Bug: 145823510
    Test: MPTS test
    Change-Id: Ie26b8e4fefea25fa818bd1eaf9df4a0bb153d42d

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ $(call inherit-product-if-exists, vendor/xiaomi/violet/violet-vendor.mk)
 # GoogleCamera
 $(call inherit-product-if-exists, packages/apps/GoogleCamera/gcam.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_prop.mk
 
