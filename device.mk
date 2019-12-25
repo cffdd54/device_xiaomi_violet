08e95915f02f930f9f1fdf27587d801c2c88e9d6
raysenlau <raysen@live.cn>
2019-12-25 09:44:05 +0800

    Violet: Import mixer_paths_overlay_static from MIUI Q
     * From "Xiaomi/davinci/davinci:10/QKQ1.190825.002/V11.0.2.0.QFJCNXM:user/release-keys"
    
    Change-Id: I03a7e6e7121de882aeae7b56d4ecf85fbe0c1fbb

--- device.mk
+++ device.mk
@@ -111,6 +111,9 @@ PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/mixer_paths_overlay_static.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/mixer_paths_overlay_static.xml
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     liba2dpoffload \
