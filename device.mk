c93abd153f1285035241dec0a3f2f50f6a457321
ZChrySA <Zacharysaroe9356@gmail.com>
2020-02-22 10:46:14 +0530

    Violet: Build QCOM Power HAL.

--- device.mk
+++ device.mk
@@ -285,7 +285,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.violet
+    power.qcom
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
